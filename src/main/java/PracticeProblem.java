7785ccaed597cc800908c4ff87448d09774ca912
richardg5 <richard.gao5@student.tdsb.on.ca>
2025-02-24 19:45:55 +0000

    a

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,8 +1,40 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
 	}
 
+	public static int[] createIntArray() {
+		int[] arr = new int[5];
+		for (int i = 0; i < 5; i++) {
+			arr[i] = i + 1;
+		}
+		return arr;
+	}
 
+	public static String[] createArray(String s1, String s2, String s3, String s4) {
+		String[] arr = {s1, s2, s3, s4};
+		return arr;
+	}
+
+	public static int findValue(int find, int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == find) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static int findThirdValue(String find, String[] arr) {
+		int count = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i].equals(find)) {
+				if (count == 3) {
+					return i;
+				}
+				count++;
+			}
+		}
+		return -1;
+	}
 }
